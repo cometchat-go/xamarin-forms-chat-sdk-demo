dbb111616f8014af4094621e9e6f824f9541a24b
jitvarpatil <jitvar.patil@inscripts.com>
2018-11-20 12:56:04 +0530

    Firebase Updated to version 7.0162-preview

--- CometChatFormSample.iOS/AppDelegate.cs
+++ CometChatFormSample.iOS/AppDelegate.cs
@@ -74,12 +74,12 @@ namespace CometChatFormSample.iOS
         {
             // Restart any tasks that were paused (or not yet started) while the application was inactive. 
             // If the application was previously in the background, optionally refresh the user interface.
-            FirebasePushNotificationManager.Connect();
+           // FirebasePushNotificationManager.Connect();
         }
 
         public override void WillTerminate(UIApplication application)
         {
-            FirebasePushNotificationManager.Disconnect();
+           // FirebasePushNotificationManager.Disconnect();
             // Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
         }
     }
